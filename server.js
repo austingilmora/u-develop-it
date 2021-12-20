f7789404ac0ff2ff74207aa820a8e86c6f599339
Austin Gilmore <austingilmora@gmail.com>
2021-12-20 17:29:05 -0500

    added ability to show all candidates, choose 1 candidate, delete, and adda candidates

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const db = mysql.createConnection(
+    {
+        host: 'localHost',
+        user: 'root',
+        password:'rY*Zn6BRafBI&1a%',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World!'
-    });
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, rows) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(rows);
+// })
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+})
 
 app.use((req, res) => {
     res.status(404).end();
